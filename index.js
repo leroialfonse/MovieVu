2168cff10751212b79ca2c9fdf7b07c0bddaedcb
brandon <brandonwhitedev@gmail.com>
2026-01-05 15:04:45 -0500

    All the code is fixed and working thanks to mentor Frank. Refactoring, styling, responsiveness now...

--- index.js
+++ index.js
@@ -1,108 +1,167 @@
-  // // Api key : 94961933
-  // // Api - OMDB : http://www.omdbapi.com/?i=tt3896198&apikey=94961933
+//   // // Api key : 94961933
+//   // // Api - OMDB : http://www.omdbapi.com/?i=tt3896198&apikey=94961933
 
-  let movies;
+//   let movies;
 
-  //   let moviesWrapper = document.querySelector('.movies');
-
-
-  // // async function main() {
-
-  // //   const moviesWrapper= document.querySelector('.movies')
-
-  // //     const movies = await fetch(`http://www.omdbapi.com/?apikey=94961933&s=alone,${searchTerm}`);
-  // //     const moviesData = await movies.json();
-  // //     //   Had to drill down one more level through the omdbapi to expose the array.
-  // //     const allMovies = moviesData.Search;
-  // //     // console.log(allMovies)
-
-  // //     const moviesListEl = document.querySelector('.movie-list');
-
-  // //     moviesListEl.innerHTML =
+//   //   let moviesWrapper = document.querySelector('.movies');
 
 
-  // //         allMovies.map((info) => movieHTML(info)
-  // //         ).slice(0,5).join('')
+//   // // async function main() {
+
+//   // //   const moviesWrapper= document.querySelector('.movies')
+
+//   // //     const movies = await fetch(`http://www.omdbapi.com/?apikey=94961933&s=alone,${searchTerm}`);
+//   // //     const moviesData = await movies.json();
+//   // //     //   Had to drill down one more level through the omdbapi to expose the array.
+//   // //     const allMovies = moviesData.Search;
+//   // //     // console.log(allMovies)
+
+//   // //     const moviesListEl = document.querySelector('.movie-list');
+
+//   // //     moviesListEl.innerHTML =
+
+
+//   // //         allMovies.map((info) => movieHTML(info)
+//   // //         ).slice(0,6).join('')
     
-  // // }
+//   // // }
 
-  // // main();
+//   // // main();
 
 
-  async function secondary(searchTerm){
+//   async function secondary(searchTerm){
+//     if (!searchTerm) {
+//       const moviesWrapper = document.querySelector('.movies')
+//       moviesWrapper.classList += ' books_loading'
+//      await getMovies();
+
+//       moviesWrapper.classList.remove('.books_loading')
+//   }
+//   else {
+//     const movies = await fetch(`http://www.omdbapi.com/?apikey=94961933&s=${searchTerm}`);
+//     const moviesData = await movies.json();
+//     //   Had to drill down one more level through the omdbapi to expose the array.
+//     const allMovies = moviesData.Search;
+//     // console.log(allMovies)
     
-    if (!searchTerm) {
-      const moviesWrapper = document.querySelector('.movies')
-      moviesWrapper.classList += ' books_loading'
-      await getMovies();
-      moviesWrapper.classList.remove('.books_loading')
-  }
-  else {
-    const movies = await fetch(`http://www.omdbapi.com/?apikey=94961933&s=${searchTerm}`);
-    const moviesData = await movies.json();
-    //   Had to drill down one more level through the omdbapi to expose the array.
-    const allMovies = moviesData.Search;
-    // console.log(allMovies)
+//     const moviesListEl = document.querySelector('.movie-list');
     
-    const moviesListEl = document.querySelector('.movie-list');
+//     moviesListEl.innerHTML =
     
-    moviesListEl.innerHTML =
-    
-    allMovies.map((info) => {
+//     allMovies.map((info) => {
       
-      return ` <div class="movie-card">
-                <div class="movie-card__container">
-                <img src=${info.Poster}>
-                  <h3>${info.Title}</h4>
-                    <p>${info.Year}</p>
-                </div>
-              </div>`
-}).slice(0,5).join('')
+//       return ` <div class="movie-card">
+//                 <div class="movie-card__container">
+//                 <img src=${info.Poster}>
+//                   <h3>${info.Title}</h4>
+//                     <p>${info.Year}</p>
+//                 </div>
+//               </div>`
+// }).slice(0,6).join('')
   
-  }}
+//   }}
 
-    setTimeout(() => {
-      getMovies();
-    }, 3000)
+//   // secondary()
+
+//     setTimeout(() => {
+//       getMovies()
+//     }, 6000)
 
 
 
-  function searchMovies(){
-    setTimeout( () => { 
-      searchTerm = document.querySelector('#search-input').value;
-          secondary(searchTerm)
-          }, 3000)
+//   function searchMovies(){
+//     setTimeout(() => { 
+//       searchTerm = document.querySelector('#search-input').value;
+//           secondary(searchTerm)
+//     }, 6000)
+//   }
+
+
+
+
+
+// async function getMovies(){
+
+
+//   const movies = await fetch(`http://www.omdbapi.com/?apikey=94961933&s=alone`);
+//   const moviesData = await movies.json();
+//   //   Had to drill down one more level through the omdbapi to expose the array.
+//   const allMovies = moviesData.Search;
+
+//   const moviesListEl = document.querySelector('.movie-list');
+
+//   moviesListEl.innerHTML =
+
+
+//      allMovies.map((info) => {
+
+//       return ` <div class="movie-card">
+//                 <div class="movie-card__container">
+//                 <img src=${info.Poster}>
+//                   <h3>${info.Title}</h4>
+//                     <p>${info.Year}</p>
+//                 </div>
+//               </div>`
+// }).slice(0,6).join('')
+
+
+// }
+
+// getMovies();
+
+
+
+
+// Api key : 94961933
+// // Api - OMDB : http://www.omdbapi.com/?i=tt3896198&apikey=94961933
+
+let movies;
+
+const moviesWrapper = document.querySelector(".movies");
+const loading = document.querySelector(".loading");
+const moviesListEl = document.querySelector(".movie-list");
+const input = document.querySelector("#search-input");
+const results = document.querySelector(".results");
+
+setTimeout(() => {
+  getMovies();
+}, 1000000);
+
+function searchMovies() {
+  let searchTerm = input.value;
+  if (!searchTerm.length > 0) {
+    alert("Please enter a movie name");
+    return;
   }
+  getMovies(searchTerm);
+}
 
-
-
-
-
-async function getMovies(){
-
-
-  const movies = await fetch(`http://www.omdbapi.com/?apikey=94961933&s=alone`);
+async function getMovies(searchTerm) {
+  loading.classList.add("movies__loading");
+  results.classList.add("movies__loading");
+  moviesListEl.innerHTML = "";
+  const movies = await fetch(
+    `http://www.omdbapi.com/?apikey=94961933&s=${searchTerm || "alone"}`
+  );
   const moviesData = await movies.json();
   //   Had to drill down one more level through the omdbapi to expose the array.
   const allMovies = moviesData.Search;
-
-  const moviesListEl = document.querySelector('.movie-list');
-
-  moviesListEl.innerHTML =
-
-
-     allMovies.map((info) => {
-
-      return ` <div class="movie-card">
-                <div class="movie-card__container">
-                <img src=${info.Poster}>
-                  <h3>${info.Title}</h4>
-                    <p>${info.Year}</p>
-                </div>
-              </div>`
-}).slice(0,5).join('')
-
-
+  setTimeout(() => {
+    moviesListEl.innerHTML = allMovies
+      .map((info) => {
+        return ` <div class="movie-card">
+                  <div class="movie-card__container">
+                  <img src=${info.Poster}>
+                    <h3>${info.Title}</h4>
+                      <p>${info.Year}</p>
+                  </div>
+                </div>`;
+      })
+      .slice(0, 6)
+      .join("");
+    loading.classList.remove("movies__loading");
+    results.classList.remove("movies__loading");
+  }, 3000);
 }
 
 getMovies();
